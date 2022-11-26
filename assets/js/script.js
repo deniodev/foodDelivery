5e9b20cc60919af28f4caf8862dcb58d36635608
Denio Nhanale <dnhanale@gmail.com>
2022-11-26 06:58:54 +0200

    fifth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ const loadingElement = document.querySelector("[data-loading-container]");
 
 window.addEventListener("load", function () {
     loadingElement.classList.add("loaded");
-    this.document.body.classList.add("loaded");
+    document.body.classList.add("loaded");
 });
 
 
